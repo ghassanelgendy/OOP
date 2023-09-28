60da0e99bf8f55d6135d3dcc6e8490ac86803dec
Ghassan <112765677+ghassanelgendy@users.noreply.github.com>
2023-09-28 19:40:45 +0300

    Add Overloading Lesson

--- main/Calculator.cpp
+++ main/Calculator.cpp
@@ -1,1 +1,32 @@
 #include "Calculator.h"
+#include <iostream>
+
+Calculator::Calculator()
+{
+	cout << "Calculator is initialized!";
+}
+
+Calculator::~Calculator()
+{
+	cout << "Bye bye ya coach";
+}
+
+float Calculator::add(float x, float y)
+{
+	return x+y;
+}
+
+int Calculator::add(int x, int y)
+{
+	return x+y;
+}
+
+int Calculator::add(int x, int y, int z)
+{
+	return x+y+z;
+}
+
+int Calculator::add(string s1, string s2)
+{
+	return s1+" " + s2);
+}
