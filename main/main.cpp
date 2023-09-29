81176a9dfc27837d80353620091fa60d6a6b3f81
Ghassan <112765677+ghassanelgendy@users.noreply.github.com>
2023-09-29 19:37:47 +0300

    Revert "Add Polymorphism"
    This reverts commit f407be914526a67d50aa2ad96734d8740df75fe0.

--- main/main.cpp
+++ main/main.cpp
@@ -9,8 +9,13 @@
 #include "Employee.h"
 #include "TeachingAssistant.h"
 
-
 int main()
 {
-	
+	TeachingAssistant TA1("Ghassan", 23, "Male", "CS", "NASA", 4, 2.69, "TA", 2300);
+	TA1.display();
+	TA1.setResearchInterest("Data Science");
+	TA1.setSalary(2400);
+	TA1.display();
+
+
 }
